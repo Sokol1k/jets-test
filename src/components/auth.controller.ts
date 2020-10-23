c0fcbe6e82d4dff1ec17d857deef2ece9197d9f2
Sokol1k <danil.sokolowskiy@gmail.com>
2020-10-23 04:35:38 +0300

    [FIX]: test for register endpoint

--- src/components/auth.controller.ts
+++ src/components/auth.controller.ts
@@ -3,8 +3,10 @@ import User from '../services/auth.service'
 
 async function register(req : Request, res : Response) : Promise<void> {
   try {
-    const data =  await User.createUser(req.body)
-    res.status(201).send(data)
+    await User.createUser(req.body)
+    res.status(201).send({
+      message: 'User has been registered!'
+    })
   } catch (error) {
     res.status(500).send(error)
   }
--- test/auth.test.ts
+++ test/auth.test.ts
@@ -3,15 +3,45 @@ import app from '../src/index'
 
 describe('Authorization endpoint', () : void => {
   it('should create a new user', async () : Promise<void> => {
-    const res : request.SuperTest<request.Test> = await request(app)
-      .post('/auth/registration')
+    const res : any = await request(app)
+      .post('/api/auth/register')
       .send({
           name: 'Bob',
           surname: 'Jones',
           email: 'bob@gmail.com',
-          password: '12345',
-          confirm_password: '12345'
+          password: '123456',
+          confirm_password: '123456'
       })
     expect(res.statusCode).toEqual(201)
+    expect(res.body.message).not.toBeUndefined()
+  }),
+  it('should return an error that email is not free', async () : Promise<void> => {
+    const res : any = await request(app)
+      .post('/api/auth/register')
+      .send({
+          name: 'Bob',
+          surname: 'Jones',
+          email: 'bob@gmail.com',
+          password: '123456',
+          confirm_password: '123456'
+      })
+    expect(res.statusCode).toEqual(403)
+    expect(res.body.email).not.toBeUndefined()
+  }),
+  it('should return an error that the validation failed', async () : Promise<void> => {
+    const res : any = await request(app)
+      .post('/api/auth/register')
+      .send({
+          name: '',
+          surname: '',
+          email: '',
+          password: '',
+          confirm_password: ''
+      })
+    expect(res.statusCode).toEqual(422)
+    expect(res.body.name).not.toBeUndefined()
+    expect(res.body.surname).not.toBeUndefined()
+    expect(res.body.email).not.toBeUndefined()
+    expect(res.body.password).not.toBeUndefined()
   })
 })
